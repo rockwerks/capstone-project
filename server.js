04e393532708fa2af50304ada858e31a6e0df731
rockwerks <anthony@rockwerks.ca>
2025-10-07 15:42:57 -0700

    refactor server setup: move server.js to root and update start script

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '../public/index.html');
+  res.sendFile(path.join(__dirname, 'build/static/index.html'));
 });
 
 app.get('/api/hello', (req, res) => {
