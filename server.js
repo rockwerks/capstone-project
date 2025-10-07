fa44189aa62cdcf60df9d9b3b6ffe480a09c9456
rockwerks <anthony@rockwerks.ca>
2025-10-07 16:54:37 -0700

    enhance API endpoint to return data from MongoDB and clean up commented code

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,24 +13,9 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 
 const app = express();
 const port = process.env.PORT || 8080;
-
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -36,8 +23,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/static/index.html'));
 });
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the API' });
+app.get('/api/hello', async (req, res) => {
+  const results = await client.db("locationscheduler").collection("itineraries").find({}).limit(5).toArray();
+  res.json({ message: 'Hello from the API', data: results });
 });
 
 app.listen(port, () => {
