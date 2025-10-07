dd0b7998aee257ed206e34feaa7ad2696ada78dd
rockwerks <anthony@rockwerks.ca>
2025-10-07 15:24:46 -0700

    update root route to serve index.html and add API endpoint for greeting

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
 });
 
 app.listen(port, () => {
